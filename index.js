1f9ee4759f120c3f85cc2c1838904f67b9188e0b
Murat Memis <3605716+Buttersail@users.noreply.github.com>
2022-03-11 09:08:53 +0100

    complete teacher code with our own

--- index.js
+++ index.js
@@ -1,38 +1,37 @@
-import { renderTemplate, setActive, showPage } from "./utils.js"
-import { setupLoginHandlers, logout, updateLoginDependentComponents } from "./js-for-pages/login.js"
-
-
-
-
+import { renderTemplate, setActive, showPage } from './utils.js'
+import { getAllCars } from './js-for-pages/seeCars.js'
+import { addAddCarHandlers } from './js-for-pages/addCar.js'
+import { setupLoginHandlers, logout, updateLoginDependentComponents } from './js-for-pages/login.js'
 
 function renderMenuItems(evt) {
   const element = evt.target
   setActive(element)
-  const id = element.id;
-  renderTemplate(id)  //This setups the HTML for the page
+  const id = element.id
+  renderTemplate(id) //This setups the HTML for the page
   switch (id) {
     //Here you can execute JavaScript for the selected page
     // case "page-1": {
     //   break
     // }
-    case "page-see-cars": {
+    case 'page-see-cars': {
+      getAllCars()
       break
     }
-    case "page-login": {
+    case 'page-add-cars': {
+      addAddCarHandlers()
+      break
+    }
+    case 'page-login': {
       setupLoginHandlers()
       break
     }
-    case "page-logout": {
+    case 'page-logout': {
       logout()
       break
     }
   }
 }
 
-document.getElementById("menu").onclick = renderMenuItems;
-showPage("page-about") //Set the default page to render
+document.getElementById('menu').onclick = renderMenuItems
+showPage('page-about') //Set the default page to render
 updateLoginDependentComponents()
-
-
-
-
--- utils.js
+++ utils.js
@@ -1,27 +1,26 @@
-
 /**
  * The encoder method we have used when inserting untrusted data via the innerHTML property
  * Ref: https://cheatsheetseries.owasp.org/cheatsheets/Cross_Site_Scripting_Prevention_Cheat_Sheet.html
- * @param {str} str 
+ * @param {str} str
  * @returns the encode string
  */
 export function encode(str) {
-  let encoded = "" + str
-  encoded = encoded.replace(/&/g, "&amp;");
-  encoded = encoded.replace(/>/g, "&gt;");
-  encoded = encoded.replace(/</g, "&lt;");
-  encoded = encoded.replace(/"/g, "&quot;");
-  encoded = encoded.replace(/'/g, "&#039;");
-  return encoded;
+  let encoded = '' + str
+  encoded = encoded.replace(/&/g, '&amp;')
+  encoded = encoded.replace(/>/g, '&gt;')
+  encoded = encoded.replace(/</g, '&lt;')
+  encoded = encoded.replace(/"/g, '&quot;')
+  encoded = encoded.replace(/'/g, '&#039;')
+  return encoded
 }
 
 export function setActive(newActive) {
-  const linkDivs = document.getElementById("menu").querySelectorAll("a")
-  linkDivs.forEach(div => {
-    div.classList.remove("active")
+  const linkDivs = document.getElementById('menu').querySelectorAll('a')
+  linkDivs.forEach((div) => {
+    div.classList.remove('active')
   })
   if (newActive) {
-    newActive.classList.add("active")
+    newActive.classList.add('active')
   }
 }
 
@@ -30,18 +29,17 @@ export function renderTemplate(id) {
   if (!temp) {
     return console.error(`No Template found for '${id}' `)
   }
-  const clon = temp.content.cloneNode(true);
-  document.getElementById("content").innerHTML = ""
-  document.getElementById("content").appendChild(clon)
+  const clon = temp.content.cloneNode(true)
+  document.getElementById('content').innerHTML = ''
+  document.getElementById('content').appendChild(clon)
 }
 
-const clickEvent = new MouseEvent("click", {
+const clickEvent = new MouseEvent('click', {
   view: window,
   bubbles: true,
-  cancelable: true
-});
+  cancelable: true,
+})
 
 export function showPage(pageId) {
   document.getElementById(pageId).dispatchEvent(clickEvent)
 }
-
